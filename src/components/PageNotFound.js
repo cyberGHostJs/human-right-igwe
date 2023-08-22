62f6d46f6fe2ac83323426efd4c5a026d52211bb
Igweokolo Davidson <davidsonigweokolo@gmail.com>
2023-08-22 14:04:10 +0100

    update

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const PageNotFound = ()=>{
     return(
         <div>
             <div>PageNotFound</div>
+            <Link to='/'>Go home</Link>
         </div>
     )
 }
